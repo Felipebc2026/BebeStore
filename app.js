1da86926b079f58281ba032abe454ab7cd461f1e
Felipebc2026 <fbrcrisostomo@gmail.com>
2026-01-06 15:53:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-fetch('productos.json')
-  .then(res => res.json())
+fetch('./productos.json')
+  .then(response => response.json())
   .then(productos => {
     const contenedor = document.getElementById('productos');
 
     productos.forEach(p => {
       const card = document.createElement('div');
-      card.className = 'card';
+      card.classList.add('card');
 
       card.innerHTML = `
-        <img src="${p.imagen}" alt="${p.nombre}" />
+        <img src="./${p.imagen}" alt="${p.nombre}">
         <h3>${p.nombre}</h3>
         <p class="precio">$${p.precio}</p>
       `;
 
       contenedor.appendChild(card);
     });
+  })
+  .catch(error => {
+    console.error('Error cargando productos:', error);
   });
